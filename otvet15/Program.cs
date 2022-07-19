40fffb8f2b693634a1ab68fd678c5ccc84b86e89
Elena <elena.tim@bk.ru>
2022-07-19 13:37:32 +0300

    ответ 15

---
+++ otvet15/Program.cs
@@ -0,0 +1,19 @@
+﻿int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber >= 1 && dayNumber <= 7 )
+{
+    if (dayNumber <=5)
+    {
+        Console.WriteLine("нет");
+    }
+        
+    if (dayNumber == 6)
+    {
+        Console.WriteLine("да");
+    }
+    if (dayNumber == 7)
+    {
+        Console.WriteLine("да");
+    }
+}
+
