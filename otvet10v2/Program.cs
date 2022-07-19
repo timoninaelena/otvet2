bfc90f3c82daf582fcde62cbcb1efdaa7fb2b6ed
Elena <elena.tim@bk.ru>
2022-07-19 11:14:42 +0300

    вторая версия задачи 10

---
+++ otvet10v2/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+int firstDigit = number / 100;
+int remains = number % 100;
+int SecondDigit = remains / 10;
+
+Console.WriteLine(SecondDigit);
+
